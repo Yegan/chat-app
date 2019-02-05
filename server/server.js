a98e9d6e4c0a61c584e9a75dc855f6d4a80a877f
Yegan <yeganj@gmail.com>
2019-02-05 22:01:06 +0200

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
-const express = require('express');
 const http = require('http');
-const port = process.env.PORT || 3000;
+const express = require('express');
 const socketIO = require('socket.io');
+
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 // var server = http.createServer((req,res)=>{
 
 // })
 
-var server = http.createServer(app);
 app.use(express.static(publicPath));
+
+
+io.on('connection', (socket)=> {
+    console.log('Never user connected')
+})
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
