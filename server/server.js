0a9d818891a9b8e85e51e0a1c097de0b18c9df18
Yegan <yeganj@gmail.com>
2019-02-05 18:12:33 +0200

    installing socketIO

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
+const http = require('http');
 const port = process.env.PORT || 3000;
+const socketIO = require('socket.io');
 var app = express();
+// var server = http.createServer((req,res)=>{
 
+// })
+
+var server = http.createServer(app);
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 });
 // console.log(__dirname + '/../public');
