d27a6c70ebed7d799051ba0923daf1db09a6b252
Yegan <yeganj@gmail.com>
2019-02-11 10:15:10 +0200

    socket emit events

--- public/js/index.js
+++ public/js/index.js
@@ -2,14 +2,28 @@ var socket = io('http://localhost:3000/');
 
 socket.on('connect',function (){
     console.log('Connected to the server')
+
+    socket.emit('createMessage', {
+        from: 'Andrew',
+        text: 'Yup, that works for me.',
+    })
+
+    // socket.emit('createEmail', {
+    //     to: 'jen@example.com',
+    //     text: 'Hey. This is Andrew.',
+    // })
 });
 
 socket.on('disconnect',function (){
     console.log('Disconnected from the server')
 });
 
-socket.on('newEmail', function (){
-console.log('New email');
-});
 
+// socket.on('newEmail', function (email){
+// console.log('New email', email);
+// });
+
+socket.on('newMessage',function(message){
+console.log('newMessage', message)
+})
 
